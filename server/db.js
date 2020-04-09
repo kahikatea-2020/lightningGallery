451c6ab1d921c591ed80f013b865f1cf57714bac
Raymond Leung <leptanian@gmail.com>
2020-04-10 08:40:45 +1200

    Add db functions

--- server/db.js
+++ server/db.js
@@ -2,3 +2,37 @@ const knex = require('knex')
 const config = require('../knexfile').development
 const connection = knex(config)
 
+// data for home page
+function getTopics(db = connection) {
+  return db('topics')
+    .select()
+}
+
+function updateTopic(id, data, db = connection) {
+  return db('topics')
+    .where('id', id)
+    .update(data)
+}
+
+function newTopic(data, db = connection) {
+  return db('topics')
+    .insert(data)
+}
+
+function getComments(data, db = connection) {
+  return db('comments')
+    .select()
+}
+
+function newComments(data, db = connection) {
+  return db('comments')
+    .insert(data)
+}
+
+module.exports = {
+  getTopics,
+  updateTopic,
+  newTopic,
+  newComments,
+  getComments
+}
